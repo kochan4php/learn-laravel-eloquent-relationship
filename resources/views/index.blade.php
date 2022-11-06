002b90fec16c185bffacbe66e0efc6da9171c525
kochan4php <aprodeosubarno@gmail.com>
2022-11-06 23:09:12 +0700

    init laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('container')
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center">Belajar Laravel Eloquent Relationship</h1>
+        </div>
+    </div>
+@endsection
